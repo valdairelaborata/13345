18542569d6921c658801a5b04810e2821015f39e
Valdair <valdairelaborata@gmail.com>
2025-07-22 20:36:30 -0300

    lab 01

---
+++ operadores.py
@@ -0,0 +1,6 @@
+
+# print (not 1 < 3)
+
+# print ((6 > 3) and (2 == 2) and (5 > 2))   
+
+# print ((2 > 3) or (3 == 2) or (1 > 2))   
--- variaveis.py
+++ variaveis.py
@@ -1,28 +1,33 @@
-a = 1
-frase = "opa!"
-valor = 12.9
-idade, profissao = 20, "programador"
+# a = 1
+# frase = "opa!"
+# valor = 12.9
+# idade, profissao = 20, "programador"
 
-bruto = 1000
-imposto = 50
+# bruto = 1000
+# imposto = 50
 
-liquido = (((2 * 5)/3) / bruto) - imposto
+# liquido = (((2 * 5)/3) / bruto) - imposto
 
 
-print(type(a))
+# print(type(a))
 
-resultado = a + 1
-print(resultado)
-a = "5"
+# resultado = a + 1
+# print(resultado)
+# a = "5"
 
-resultado = a + "1"
-print(resultado)
+# resultado = a + "1"
+# print(resultado)
 
-print(type(a))
+# print(type(a))
 
-# print(type(frase))
-# print(type(valor))
+# # print(type(frase))
+# # print(type(valor))
 
-# teste
+# # teste
+
+
+idade = None
+
+print(idade)
 
 
