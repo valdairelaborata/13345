4a8cfdceb99cf63f9d4b6cc1a79427cfc615a0c6
Valdair <valdairelaborata@gmail.com>
2025-07-23 20:58:41 -0300

    lab_02

---
+++ lista.py
@@ -0,0 +1,19 @@
+
+# lista = ["abacate", "manga", "abacaxi"]
+
+lista = [True, 10, "manga", [10, 5, "teste"], 3.6]
+
+# print(lista[3][1])
+
+# existe_manga = "abacate" in lista
+# print(existe_manga)
+
+# lista.append("bacate")
+
+# lista.insert(2, "abacate")
+
+# print(lista.pop())
+
+# lista.remove("manga")
+
+print(lista)
