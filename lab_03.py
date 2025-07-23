0c92053631a02c042567f467350d993ef6562d52
Valdair <valdairelaborata@gmail.com>
2025-07-22 21:43:21 -0300

    lab 03

---
+++ lab_03.py
@@ -0,0 +1,6 @@
+primeiro_numero = input("Digite o primeiro número: ")
+segundo_numero = input("Digite o segundo número: ")
+
+resultado = int(primeiro_numero) ** int(segundo_numero) 
+
+print("O resultado é: " + str(resultado))
