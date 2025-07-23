7e3df601fe000f375d9fd8fe5b4c6206cbb4c610
Valdair <valdairelaborata@gmail.com>
2025-07-22 21:06:54 -0300

    lab_01

---
+++ lab_01.py
@@ -0,0 +1,8 @@
+
+
+primeiro_numero = input("Informe o primeiro número: ")
+segundo_numero = input("Informe o segundo número: ")
+
+soma = int(primeiro_numero) + int(segundo_numero)
+
+print("A soma é : " + str(soma))
