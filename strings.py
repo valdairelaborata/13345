1e42eef0a5087d1500f2326fb62998ac971c6081
Valdair <valdairelaborata@gmail.com>
2025-07-23 20:05:16 -0300

    string

---
+++ strings.py
@@ -0,0 +1,10 @@
+x = 'Algum valor'
+
+link = "<a href='#'> clique aqui para ir .... </a>"
+
+
+varias_linhas ="""  
+                    valor da linha 1
+                    valor da linha 2
+                    valor da linha 3 """
+
