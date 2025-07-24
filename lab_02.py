7f4b2c94dc7a64c67152c96189c31391bb8e4ecb
Valdair <valdairelaborata@gmail.com>
2025-07-23 21:44:48 -0300

    lab_02

---
+++ lab_02.py
@@ -0,0 +1,22 @@
+
+#lista = [10, 2, 5, 20, 60]
+
+# lista.sort()
+# print(lista)
+
+# lista.sort(reverse=True)
+# print(lista)
+
+
+lista = [10, 2, 20, 3, 20, 60]
+
+sub_lista = lista[2:4]
+
+print(sub_lista)
+
+
+
+
+
+
+
