3552dc06a113f81979551a66b374e0513500889e
Valdair <valdairelaborata@gmail.com>
2025-07-24 20:01:53 -0300

    lab_03

---
+++ dicionario.py
@@ -0,0 +1,36 @@
+cliente = """ {
+    "nome": "Batman",
+    "telefone":41999866081, 
+    "email": "emersonmetalar@gmail.com"} """
+ 
+# nome = cliente["endereco"]
+
+
+# nome = cliente.get("telefone", "Valor não encontrado!")
+
+# cliente["endereco"] = "Batcaverna"
+
+# cliente["telefone"] = [646545464, 88888888]
+
+# del cliente["telefone"]
+
+# email = cliente.pop("email", "Valor não encontrado!")
+
+# dicionario = eval(cliente)
+
+
+endereco = {"endereco" : "Batcaverna"}
+
+cliente["endereco"] = "Batcaverna"
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ lab_03.py
@@ -0,0 +1,2 @@
+1 - Criar um dicionário que armazene informações sobre uma pessoa (nome, idade, e cidade) e exiba essas informações.
+2 - Criar um dicionário que contenha outros dicionários como valores e acessar os elementos.
