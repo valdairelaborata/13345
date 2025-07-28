0a9d168774b67800a0f3500cec77bb94dd7a00d6
Valdair <valdairelaborata@gmail.com>
2025-07-28 19:37:57 -0300

    lab_03

---
+++ for.py
@@ -0,0 +1,20 @@
+# nomes = ["Nome 1", "Nome 2", "Nome 3"]
+
+# for nome in nomes:
+#     if nome == "Nome 2":
+#         break
+#         # continue
+#     else:
+#         print(nome)  
+
+# frutas = ["abacate", "abacaxi", "amora", "pêra"]
+
+# for indice, fruta in enumerate(frutas):
+#     print(f"indice {indice} => fruta {fruta}") 
+
+
+numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+pares = [n for n in numeros if n % 2 == 0]
+
+print(pares)
