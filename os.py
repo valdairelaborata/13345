09252c4488215c0d831dbbf04cfcf413e2d10f55
Valdair <valdairelaborata@gmail.com>
2025-07-31 19:44:35 -0300

    lab_01

---
+++ os.py
@@ -0,0 +1,69 @@
+import os
+
+# ambiente = os.getcwd()
+
+# print(ambiente)
+
+# ambiente = os.listdir()
+
+# print(ambiente)
+
+# inicio = os.getcwd()
+
+# for root, dirs, files in os.walk(inicio):
+#     print("Root: " + str(root ))
+#     print("Diretórios: " + str(dirs))
+#     print("Arquivos: " + str(files))
+
+# ambiente = os.getcwd()
+
+# print("Inicial: " + str(ambiente))
+
+# os.chdir("projeto")
+
+# ambiente = os.getcwd()
+
+# print("Final: " + str(ambiente))
+
+# print("Antes: " + str(os.listdir()))
+
+# os.mkdir("diretorio")
+
+# print("Depois: " + str(os.listdir()))
+
+# print("Antes: " + str(os.listdir()))
+
+# os.makedirs("bin/arquivos")
+
+# print("Depois: " + str(os.listdir()))
+
+# print("Antes: " + str(os.listdir()))
+
+# os.rmdir("diretorio")
+
+# print("Depois: " + str(os.listdir()))
+
+
+# print("Antes: " + str(os.listdir()))
+
+# os.removedirs("bin/arquivos")
+
+# print("Depois: " + str(os.listdir()))
+
+# script = __file__
+
+# print(script)
+
+# base = os.path.basename(__file__)
+
+# print(base)
+
+
+# base = os.path.dirname(__file__)
+
+# print(base)
+
+base = os.path.abspath(__file__)
+
+print(base)
+
